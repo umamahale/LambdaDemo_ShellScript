f2d5b4e4b64d244a94e6d4f74d258f38b96b8f52
umamahale <umagasmos@gmail.com>
2021-10-18 07:28:38 -0400

    first message

---
+++ Person.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LamdaExpressionExample
+{
+    class Person
+    {
+        internal static string name;
+        public string SSN;
+        public string Name;
+        public string Address;
+        public int Age;
+        public Person(string ssn, string name,string add,int age)
+        {
+            SSN = ssn;
+            Name = name;
+            Address = add;
+            Age = age;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LamdaExpressionExample
+{
+    class Program
+    {
+       
+        static void Main(string[] args)
+        {
+           
+           
+            List<Person> listperson = new List<Person>();
+          
+            AddRecords(listperson);
+
+        }
+        private static void AddRecords(List<Person> listperson)
+        {
+          
+            {
+                listperson.Add(new Person("1232132", "uma", "12 Main Street", 15));
+                listperson.Add(new Person("1232111", "rohit", "13 Main Street", 25));
+                listperson.Add(new Person("5555555", "jhon", "14 Main Street", 35));
+                listperson.Add(new Person("6978888", "reshu", "15 Main Street", 65));
+                listperson.Add(new Person("8111111", "riya", "16 Main Street", 55));
+
+            }
+            private static void
+            {
+                Console.WriteLine("name" + Person.name);
+            }
+
+        }
+    }
+}
